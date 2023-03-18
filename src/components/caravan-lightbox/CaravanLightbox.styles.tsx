b3f454e79a85ae07f1b415923eedb5fd90272a3b
Marek Matušica <83821425+mmm-marek@users.noreply.github.com>
2023-03-18 21:40:33 +0100

    feat: responsive styling for lightbox added

--- src/components/caravan-lightbox/CaravanLightbox.styles.tsx
+++ src/components/caravan-lightbox/CaravanLightbox.styles.tsx
@@ -9,14 +9,20 @@ export const StyledButton = styled.button`
     background-color: ${({ theme }) => theme.colors.darkGrey};
     color: ${({ theme }) => theme.colors.white};
     position: relative;
-    width: 4.25rem;
-    height: 4.25rem;
+    width: 2.25rem;
+    height: 2.25rem;
     border-radius: 50%;
     clip-path: circle();
-    font-size: 30px;
+    font-size: 16px;
     font-weight: 700;
     z-index: 10;
     cursor: pointer;
     margin-inline: 1rem;
     border: 0;
+
+    @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+        width: 4.25rem;
+        height: 4.25rem;
+        font-size: 30px;
+    }
 `;
