deec6fb59c41a72bc5d4edfd617bd3851d918d11
Marek Matušica <83821425+mmm-marek@users.noreply.github.com>
2023-03-19 10:33:32 +0100

    refactor

--- src/components/icon/Icon.component.tsx
+++ src/components/icon/Icon.component.tsx
@@ -2,10 +2,10 @@ import { StyledImage } from "./Icon.styles";
 
 type IconProps = {
     name: "arrow" | "bed" | "bolt" | "person" | "shower" | "toilet";
-    rotateangle?: number;
+    rotate?: number;
 };
 
-export const Icon = ({ name, rotateangle }: IconProps) => {
+export const Icon = ({ name, rotate }: IconProps) => {
     const width = name === "bolt" ? 16 : name === "arrow" ? 22 : 20;
     const height = name === "bolt" ? 16 : name === "arrow" ? 22 : 20;
     return (
@@ -14,7 +14,7 @@ export const Icon = ({ name, rotateangle }: IconProps) => {
             width={width}
             height={height}
             alt={name}
-            rotateangle={rotateangle || 0}
+            rotate={rotate || 0}
         />
     );
 };
--- src/components/icon/Icon.styles.tsx
+++ src/components/icon/Icon.styles.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Image from "next/image";
 
-export const StyledImage = styled(Image)<{ rotateangle: number }>`
-    transform: rotate(${(props) => props.rotateangle}deg);
+export const StyledImage = styled(Image)<{ rotate: number }>`
+    transform: rotate(${(props) => props.rotate}deg);
 `;
